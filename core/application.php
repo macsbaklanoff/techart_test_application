07eb34f381d2e56d4f07dd7d22afdd48f9654cb5
macsbaklanoff <baklanov689@gmail.com>
2025-08-18 15:26:20 +0300

    refactor files

--- core/application.php
+++ core/application.php
@@ -1,23 +1,16 @@
 <?php
-require_once 'request.php';
-require_once 'response.php';
 require_once 'route.php';
 
 class Application {
   protected string $uri;
 
-  public Request $request;
-  public Response $response;
 
   public Route $route;
 
   public Model $model;
 
   public function __construct() {
-    $this->uri = $_SERVER['REQUEST_URI'];
-    $this->request = new Request($this->uri);
-    $this->response = new Response();
-    $this->route = new Route($this->request, $this->response);
+    $this->route = new Route();
 
     $this->model = new Model();
   }
