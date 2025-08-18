0c13c2cd7ff0978bcfe62a804132f67c52776d93
macsbaklanoff <baklanov689@gmail.com>
2025-08-18 16:42:08 +0300

    update code

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 ini_set('display_errors', 1);
 require_once 'core/application.php';
 require_once 'core/model.php';
-// require_once 'core/view.php';
-// require_once 'core/controller.php';
 require_once 'core/route.php';
 
 
