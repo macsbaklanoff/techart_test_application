7931aabce34a3b7fe28cb548e1ae4592db598e04
macsbaklanoff <baklanov689@gmail.com>
2025-09-01 17:33:44 +0300

    update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function autoloader ($class_name) {
     // $namespaces_array = explode('\\', $class_name); // имя класса приходит с пространством имен
     // $class_name = $namespaces_array[count($namespaces_array) - 1]; //нашли имя класса
     $filename = $class_name . '.php';
-    var_dump($class_name . '.php');
+    var_dump($class_name . '.php');//
 
     if (file_exists($filename)) {
         require_once $filename;
